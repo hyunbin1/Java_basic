d6d76f4e504bb4585d8ff8437662e179c04152d9
hyunbin1 <kimgusqls1@gmail.com>
2021-06-15 10:52:58 +0900

    21/06/15 - study java basic content

---
+++ Ch2/src/Ex2_1.java
@@ -0,0 +1,12 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5+3); // 더하기
+		System.out.println(5-3); //빼기
+		System.out.println(5*3); //곱하기
+		System.out.println(5/3.0); //나누기
+	}
+
+}
