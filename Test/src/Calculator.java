4093044da6653422fae5f19c913cb8e2a732e3ee
hyunbin1 <kimgusqls1@gmail.com>
2021-06-27 21:52:13 +0900

    21/6/27 계산기 만들었땅 ㅎㅎㅎ

--- Test/src/Calculator.java
+++ Test/src/Calculator.java
@@ -1,29 +1,49 @@
+import java.util.Scanner;
+
 public class Calculator {
-
-
     public static void main(String[] args){
 
-        divide(3, 0);
+        //결과값 선언
+        double result;
 
-    }
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. add, 2. minus, 3. multiply, 4. divide");
+        System.out.println("원하는 번호를 선택하세요.");
+        int start = input.nextInt();
+        if(!(start <= 1 && start <= 4)){
+            System.out.println("1~4이내의 값을 입력해주세요.");
+        }else {
 
-    static int add(int a, int b){
-       return a+b;
-    };
+            System.out.println("첫 번째 번호를 입력하세요");
+            Double firstValue = input.nextDouble();
 
-    static int multiply(int a, int b){
-        return a*b;
-    }
-    static void divide(int a, double b) {
-        if (b == 0) {
-            System.out.println("0이 아닐세");
-        } else {
-            System.out.println(a/b);
+            System.out.println("두 번째 번호를 입력하세요");
+            Double secondValue = input.nextDouble();
+            if (secondValue <= 0 || !(start <= 1 && start <= 4)) {
+                System.out.println("0 이상의 값을 입력해주세요");
+            }
+
+            switch (start) {
+                case 1 -> {
+                    result = firstValue + secondValue;
+                    System.out.println(firstValue + "+" + secondValue + "=" + result);
+                }
+                case 2 -> {
+                    result = firstValue - secondValue;
+                    System.out.println(firstValue + "-" + secondValue + "=" + result);
+                }
+                case 3 -> {
+                    result = firstValue * secondValue;
+                    System.out.println(firstValue + "*" + secondValue + "=" + result);
+                }
+                case 4 -> {
+                    result = firstValue / secondValue;
+                    System.out.println(firstValue + "/" + secondValue + "=" + result);
+                }
+                default -> System.out.println("1~4이내의 값을 입력해주세요.");
+            }
+            input.close();
         }
     }
-
-
 }
-
-
 
